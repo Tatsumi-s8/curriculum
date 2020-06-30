d820f9acf9decce2f66a870edb3236e6727b4a97
t.sato <t-sato@y-i-group.co.jp>
2020-06-30 14:16:30 +0900

    変数とデータ型
    「2-2/index.php」の提出

---
+++ 2-2/index.php
@@ -0,0 +1,43 @@
+<?php
+
+//カリキュラムメモ
+
+$message = "Hello World!";
+var_dump ($message);
+echo '<br>';
+
+
+$x = 8;
+$y = 5.55;
+var_dump ($x);
+echo '<br>';
+var_dump ($y);
+
+echo '<br>';
+$flag = true;
+var_dump ($flag);
+
+echo '<br>';
+$emp = null;
+var_dump($emp);
+
+echo '<br>';
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
+
+
+
+//IT用語調べ
+
+echo '<br>';
+echo '1. API'.'<br>';
+echo 'ソフトウェアやアプリケーションなどの一部を外部に向けて公開することにより、第三者が開発したソフトウェアと機能を共有できるようにしてくれるもの。'.'<br>'.
+     'USBは外部デバイスとパソコンを繋ぐインターフェースだが、APIはソフトウェア同士を繋げる。つまり、異なるソフトウェアやサービス間で認証機能を共有したり、チャット機能を共有したり、片方から数値データを取り込み、別のプログラムでそのデータを解析したりできるようになる。'.'<br>';
+echo '2. SVN（Subversion）'.'<br>';
+echo 'バージョン管理システムのひとつであり、ファイルの変更履歴を管理してくれるシステム。1つの中央リポジトリに複数の開発者がアクセスするアーキテクチャを採用している。';
+
+?>
+
